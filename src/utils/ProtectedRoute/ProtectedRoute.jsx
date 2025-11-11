7d96fe768fb9a8a4f46f143b42d9a5e522b73efa
whitecat <berndtdenniscanaya@gmail.com>
2025-11-12 02:29:04 +0800

    chore: add error dialog for restricted users

--- src/utils/ProtectedRoute/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute/ProtectedRoute.jsx
@@ -6,10 +6,12 @@ import { CircleLoader } from "react-spinners";
 import Swal from "sweetalert2";
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import { updateAdminActivity } from "../services/firebase/users.services";
+import { getUserInfoFromFirestore } from "../controller/users.controller";
 
 function  ProtectedRoute() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [userDoc, setUserDoc] = useState(null);
 
   const override = {
     display: "block",
@@ -21,8 +23,16 @@ function  ProtectedRoute() {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
       setUser(firebaseUser); // Firebase restores session here
+
+       if (firebaseUser) {
+              const result = await getUserInfoFromFirestore(firebaseUser.uid);
+              if (result.status === 200) {
+                setUserDoc(result.data);
+                setLoading(false);
+              }
+            }
       setLoading(false);
     });
     return () => unsubscribe();
@@ -38,13 +48,61 @@ function  ProtectedRoute() {
     return <CircleLoader cssOverride={override} />;
   }
 
+  //For restricting users if restricted by admin
 
+  if (userDoc?.restricted) {
+    signOut(auth);
+    return(
+    <Dialog open={userDoc?.restricted} aria-labelledby="alert-dialog-restricted-title" aria-describedby="alert-dialog-restricted-description">
+        <DialogTitle>Account Restricted</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Your account has been restricted by the administrator. Please contact support for more information.
+          </DialogContentText>
+      </DialogContent>  
+      <DialogActions>
+        <Button
+          variant="contained"
+        sx={{
+          background: "linear-gradient(90deg, #2ED573 0%, #7BED9F 100%)",
+                color: "#fff",
+                "&:hover": {
+                  background:
+                    "linear-gradient(90deg, #7BED9F 0%, #2ED573 100%)",
+                },
+        }} onClick={() => window.location.href = "/login"} color="primary" autoFocus>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+    );
+  }
 
-  // if (!user) {
-  //   // 🚫 no replace → user can still go back after logging in
-  //   // Use replace to prevent navigation flooding
-  //   return <Navigate to="/login" replace />;
-  // }
+  if (!user?.uid) {
+    return(
+      <Dialog open={!user?.uid} aria-labelledby="alert-dialog-no-user-id-title" aria-describedby="alert-dialog-no-user-id-description">
+        <DialogTitle>An Error Occurred</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            An unexpected error occurred. Please log in again.          </DialogContentText>
+      </DialogContent>  
+      <DialogActions>
+        <Button
+          variant="contained"
+        sx={{
+          background: "linear-gradient(90deg, #2ED573 0%, #7BED9F 100%)",
+                color: "#fff",
+                "&:hover": {
+                  background:
+                    "linear-gradient(90deg, #7BED9F 0%, #2ED573 100%)",
+                },
+        }} onClick={() => window.location.href = "/login"} color="primary" autoFocus>
+          OK
+        </Button>
+      </DialogActions>
+    </Dialog>
+    );
+  }
 
   if(!user?.emailVerified){
     signOut(auth);
