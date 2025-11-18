78c8af12b7a89f831bf75b0683942fbef251c3ec
whitecat <berndtdenniscanaya@gmail.com>
2025-11-19 04:05:54 +0800

    chore: added back button to terms and conditions

--- src/Pages/CMS/subpages/TermsAndConditions.jsx
+++ src/Pages/CMS/subpages/TermsAndConditions.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import {
+  Box,
+  Fade,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { useNavigate } from "react-router-dom";
 
 function TermsAndConditions() {
+  const navigate = useNavigate();
   return (
-    <div>TermsAndConditions</div>
+    <Fade in={true} timeout={500}>
+     <Box sx={{ height: "100dvh", p: 3 }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            mb: 2,
+          }}
+        >
+          <IconButton
+          onClick={()=> navigate(-1)}>
+            <ChevronLeftIcon sx={{
+              fontSize: 80,
+              color: "black"
+            }} />
+          </IconButton>
+          <Typography
+            variant="h2"
+            sx={{ fontWeight: "bold", fontFamily: '"Poppins", sans-serif' }}
+          >
+            
+            Terms and Conditions
+          </Typography>
+        </Box>
+      </Box>
+    </Fade>
   )
 }
 
